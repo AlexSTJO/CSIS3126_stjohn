22b943c1eb81b19ab163c80c855ef638d1268127
Alex St John <alexandros.georgakoudi@gmail.com>
2025-01-30 20:42:12 -0500

    Began Developing Cloud Set Up Function, created vpc configuration method, to be tested

--- orca-backend/CloudHandler.py
+++ orca-backend/CloudHandler.py
@@ -3,7 +3,7 @@ import boto3
 import time
 import os
 # Session Creator
-def session_create(access_key, secret_access_key,):
+def session_create(access_key, secret_access_key):
     session = boto3.Session(
         aws_access_key_id=access_key,
         aws_secret_access_key= secret_access_key,
@@ -70,6 +70,7 @@ def ec2_connect(session, bucket_name, ec2_instance, file_path):
                     print(stderr)
                     return stderr
 
+# Gets Cloud Credentials For Link
 def get_db_credentials():
     client = boto3.client('secretsmanager', region_name='us-east-2')
 
@@ -84,7 +85,7 @@ def get_db_credentials():
     except Exception as e:
         print(f"Error retrieving secrets: {e}")
         raise
-
+# Gets Encryption Key from Secrets Manager
 def get_encryption_key(encryption_key_id):
     secret_name = os.getenv("SECRET_NAME")
     if secret_name != "":
@@ -102,8 +103,110 @@ def get_encryption_key(encryption_key_id):
     else:
         return os.getenv("E_KEY")
     
-if __name__ == "__main__":
-    print(get_encryption_key())
+
+
+def cloud_setup(bucket_name, ec2_name, access_key, secret_access_key, region_name):
+    session = session_create(access_key, secret_access_key)
     
+    bucket_success = bucket_create(session, bucket_name, region_name)
+    
+
+def bucket_create(session, bucket_name, region_name):
+
+    s3_client = session.client("s3", region_name=region_name)
+    
+    try:
+        response = s3_client.create_bucket(Bucket=bucket_name)
+        print("Bucket created successfully", response)
+
+    except botocore.exceptions.ClientError as e:
+        error_code  = e.response["Error"]["Code"]
+
+    if error_code == "BucketAlreadyExists":
+        print(f"Error: The bucket name '{bucket_name}' is already taken and owned by another AWS account.")
+    elif error_code == "BucketAlreadyOwnedByYou":
+        print(f"Error: The bucket '{bucket_name}' already exists in your account.")
+    else:
+        print(f"Unexpected Error: {e}")
+
+
+def ec2_create(session, region_name):
+    ec2_client = session.client("ec2", region_name=region_name)
+        
+    # Create security group
+    create_and_configure_vpc(ec2_client, region_name)
+    '''try:
+        response = ec2_client.create_security_group(
+        GroupName='orca-sg',
+        Description='Security Group for Orca EC2',
+        )
+    except botocore.exceptions.ClientError as e:
+        print(f"Error: {e}")'''
+    # Create IAM Role
+    # Create Key Pair
+    # Create EC2
+
+def create_and_configure_vpc(ec2_client, region_name):
+    try:
+        response = ec2_client.create_vpc(CidrBlock='10.0.0.0/16')
+        vpc_id = response["Vpc"]["VpcID"]
+        print(f"VPC Created: {vpc_id}")
+        
+        ec2_client.modify_vpc_attribute(VpcId=vpc_id, EnableDnsSupport={'Value': True})
+        ec2_client.modify_vpc_attribute(VpcId=vpc_id, EnableDnsHostnames={'Value': True})
+    except botocore.exceptions.ClientError as e:
+        print(f"Error: {e}")
+
+    try:
+        igw_response = ec2_client.create_internet_gateway()
+        internet_gateway_id = igw_response['InternetGateway']['InternetGatewayId']
+        print(f"Internet Gateway created: {internet_gateway_id}")
+       
+        # Create a subnet in the VPC
+        subnet_response = ec2_client.create_subnet(
+            VpcId=vpc_id,
+            CidrBlock='9.0.1.0/24',
+            AvailabilityZone=f'{region_name}a'  
+        )
+
+        subnet_id = subnet_response['Subnet']['SubnetId']
+        print(f"Subnet created: {subnet_id}")
+        
+        route_table_response = ec2_client.create_route_table(VpcId=vpc_id)
+        route_table_id = route_table_response["RouteTable"]["RouteTableId"]
+        print(f"Route table created: {route_table_id}")
+        
+        ec2_client.create_route(
+            RouteTableId=route_table_id,
+            DestinationCidrBlock="0.0.0.0/0",
+            GatewayId=internet_gateway_id
+        )
+        
+        ec2_client.associate_route_table(SubnetID=subnet_id, RouteTableId=route_table_id)
+        print("Route table associated with the subnet")
+
+        ec2_client.modify_subnet_attribute(SubnetId=subnet_id, MapPublicIpOnLaunch={'Value': True})
+        
+        print("Auto-assign public IP enabled for the subnet.")
+        print("VPC Configurations Done")
+
+    except botocore.exceptions.ClientError as e:
+        print("Error: {e}")
+
+          
+        
+ 
+def pull_creds():
+    with open('../secrets.csv', newline='', encoding='utf-8-sig') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            creds = {"access_key": row["Access key ID"],
+                     "secret_access_key": row["Secret access key"]}
+
+    return creds
+
+if __name__ == "__main__":
+    creds = pull_creds()
+    session_create(creds["access_key"], creds["secret_access_key"])
 
 
